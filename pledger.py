5aae32848ed2f354b0b6883059b9df32ebcf9394
Arthur Williams <taaparthur@gmail.com>
2020-12-13 02:03:18 -0600

    Adding basic features

--- pledger.py
+++ pledger.py
@@ -1,4 +1,9 @@
+import argparse
+import os
 import re
+from string import whitespace
+import logging
+from decimal import Decimal
 
 currency_regex = re.compile("[^\d\.\-\(\)\*\+\-/ ]+")
 
@@ -42,51 +47,92 @@ class Account:
         return parent
 
     def addValue(self, currency, value, init=False):
-        if init:
-            self.values[currency] = value
-        else:
-            self.values[currency] = self.values.get(currency, 0) + value
+        if value:
+            assert isinstance(value, Decimal)
+            if init:
+                self.values[currency] = value
+            else:
+                self.values[currency] = self.values.get(currency, 0) + value
+
+    def __getValue(self, currency):
+        return self.values.get(currency, 0) + sum([children.__getValue(currency) for children in self.children.values()])
+
+    def getSpecificValue(self, currency):
+        return self.values.get(currency, 0)
 
     def getValue(self, currency):
-        return self.values.get(currency, 0) + sum([children.getValue(currency) for children in self.children.values()])
+        return self.__getValue(currency)
+
+    def getCurrencies(self):
+        return self.values.keys()
+
+    def matches(self, filter_strs):
+        if not filter_strs:
+            return True
+        for filterStr in filter_strs:
+            if re.match(filterStr, self.getProperName()):
+                return True
 
 
 class TransactionItem:
-    def __init__(self, account, token=None):
-        self.values = {}
+    def __init__(self, account, token=None, line_num=None):
         self.account = account
+        self.values = {}
+        self.postAccountValue = {}
         self.finalValue = None
+        self.line_num = line_num
         self.__parse(token)
+
+    def __str__(self):
+        return "#{} {} {}".format(self.line_num, self.account.getProperName(), self.values)
 
     def __parse(self, token):
         if token:
             if "@" in token:
                 parts = token.split("@")
-                value = float(currency_regex.sub("", parts[0]))
-                self.values[getCurrencySymbol(parts[0])] = value
-                self.values[getCurrencySymbol(parts[1])] = value * float(currency_regex.sub("", parts[1])) * -1
+                value = Decimal(currency_regex.sub("", parts[0]))
+                self.setValue(getCurrencySymbol(parts[0]), value)
+                self.setValue(getCurrencySymbol(parts[1]), value * Decimal(currency_regex.sub("", parts[1])) * -1)
             elif "=" in token:
                 parts = token.split("=")
 
-                self.finalValue = getCurrencySymbol(parts[1]), float(currency_regex.sub("", parts[1]))
+                self.finalValue = getCurrencySymbol(parts[1]), Decimal(currency_regex.sub("", parts[1]))
                 if parts[0]:
                     self.__parse(parts[0])
-                else:
-                    c, v = self.finalValue
-                    self.values[c] = v - self.account.getValue(c)
             elif "(" in token:
-                self.values[getCurrencySymbol(token)] = eval(currency_regex.sub("", token))
+                self.setValue(getCurrencySymbol(token), eval(currency_regex.sub("", token)))
             else:
-                self.values[getCurrencySymbol(token)] = float(currency_regex.sub("", token))
+                self.setValue(getCurrencySymbol(token), currency_regex.sub("", token))
 
     def getValue(self, currency):
         return self.values.get(currency, 0)
 
     def setValue(self, currency, value):
-        self.values[currency] = value
+        assert currency not in self.values
+        self.values[currency] = Decimal(value)
+
+    def setPostAccountValue(self, c, value):
+        self.postAccountValue[c] = value
+
+    def getPostAccountValue(self, c):
+        return self.postAccountValue[c]
 
     def isMissingValue(self):
         return not self.values
+
+    def hasImplicitValue(self):
+        return self.isMissingValue() and not self.finalValue
+
+    def getCurrencies(self):
+        return self.values.keys() if not self.finalValue else [self.finalValue[0]]
+
+    def isNetZero(self):
+        return len(self.values.keys()) > 1
+
+    def computeValueIfMissing(self):
+        if self.isMissingValue() and self.finalValue:
+            c, v = self.finalValue
+            self.setValue(c, v - self.account.getValue(c))
 
     def postVerify(self):
         if self.finalValue:
@@ -108,36 +154,125 @@ class Transaction:
     def __lt__(self, other):
         return self.date < other.date
 
-    def addItem(self, accountName, token=None):
+    def getHeader(self):
+        return "{} {}".format(self.date, self.title)
+
+    def __repr__(self):
+        return self.getHeader()
+
+    def dump(self):
+        print("Dumping: {} and {} items".format(self, len(self.items)))
+        for item in self.items:
+            print(item)
+
+    def addItem(self, accountName, token=None, line_num=None):
         account = self.root.getAccount(accountName)
-        item = TransactionItem(account, token)
+        item = TransactionItem(account, token, line_num)
         self.items.append(item)
-        if(item.isMissingValue()):
+        if item.hasImplicitValue():
             assert self.inferred_item is None
             self.inferred_item = item
 
     def commit(self):
-        currencies = {c for item in self.items for c in item.values.keys()}
+        logging.debug("Committing %s", self.getHeader())
+        currencies = {c for item in self.items for c in item.getCurrencies()}
+        for item in self.items:
+            item.computeValueIfMissing()
         for c in currencies:
             if not self.initialize:
-                s = sum([item.getValue(c) for item in self.items])
+                s = sum([item.getValue(c) for item in self.items if not item.isNetZero()])
                 if self.inferred_item:
                     self.inferred_item.setValue(c, -s)
                     s += -s
-                assert s == 0
+                if s != 0:
+                    logging.error("Transaction doesn't balance %d '%s' %s", s, c, [item.getValue(c) for item in self.items])
+
+                assert s == 0, str(s)
             for item in self.items:
                 item.account.addValue(c, item.getValue(c), self.initialize)
-        for item in self.items:
-            item.postVerify()
+                item.setPostAccountValue(c, item.account.getValue(c))
+                item.postVerify()
 
 
-def main(fileName="ledger"):
-    with open(fileName, "r") as f:
-        for line in f:
-            parts = line.split(";")
-            if parts[0]:
-                pass
+def recursive_print(parent, filterStr, level=1, running_total=None):
+    for account in parent.children.values():
+        if not filterStr or account.matches(filterStr):
+            for c in account.getCurrencies():
+                if account.getValue(c):
+                    if not running_total is None:
+                        running_total[c] = running_total.get(c, 0) + account.getSpecificValue(c)
+
+                    print("{:-12.2f} {:5s} {}".format(account.getValue(c), c, account.getProperName()))
+        recursive_print(account, filterStr, level + 1, running_total)
+
+
+def balance(root, transactions, filterStr=None):
+    running_total = {}
+    recursive_print(root, filterStr, running_total=running_total)
+    for c in running_total:
+        print("{:-12.4f} {:5s}".format(running_total[c], c))
+    return running_total
+
+
+def register(root, transactions, filterStr=None):
+    for transaction in transactions:
+        for item in transaction.items:
+            for c in item.getCurrencies():
+                print("{}\t{}\t{}\t{}".format(transaction.getHeader(), item.account.getProperName(), item.getValue(c), item.getPostAccountValue(c)))
+
+
+def parse_args(args=None, lines=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", default=os.getenv("LEDGER_FILE"))
+    parser.add_argument("--start")
+    parser.add_argument("--end")
+    parser.add_argument("type")
+    parser.add_argument("accounts", nargs="*")
+    namespace = parser.parse_args(args)
+    ledger_file = namespace.file
+
+    if lines:
+        root, transactions = parse_file(lines)
+    else:
+        with open(ledger_file, "r") as f:
+            root, transactions = parse_file(f)
+
+    for func in [balance, register]:
+        if func.__name__.startswith(namespace.type):
+            func(root, transactions, namespace.accounts)
+            break
+
+
+def parse_file(f, root=Account()):
+
+    transactions = []
+    t = None
+    for i, line in enumerate(f):
+        try:
+            commentSplit = line.split(";")
+            data, comment = commentSplit[0], commentSplit[1:]
+            if data.strip():
+                itemStr = data.split()
+                firstChar = data[0]
+                if data[0] in whitespace:
+                    t.addItem(itemStr[0], " ".join(itemStr[1:]), line_num=i)
+                else:
+                    t = Transaction(date=itemStr[0], title=" ".join(itemStr[1:]), root=root)
+                    transactions.append(t)
+        except Exception as e:
+            logging.error("Error processing line #%d %s", i, line)
+            raise e
+
+    for t in transactions:
+        try:
+            t.commit()
+        except ValueError:
+            t.dump()
+            raise
+
+    return root, transactions
 
 
 if __name__ == "__main__":
-    pass
+    logging.basicConfig(format='[%(filename)s:%(lineno)s]%(levelname)s:%(message)s', level=logging.DEBUG)
+    parse_args()
--- unit_tests.py
+++ unit_tests.py
@@ -1,5 +1,7 @@
 import unittest
 from .pledger import *
+import logging
+from decimal import Decimal
 
 
 class CurrencyTest(unittest.TestCase):
@@ -13,6 +15,12 @@ class TransactionTest(unittest.TestCase):
     def setUp(self):
         self.root = Account()
 
+    def assertEqual(self, A, B):
+        if isinstance(A, Decimal):
+            super().assertEqual(A.quantize(Decimal(".0000000001")), Decimal(round(B, 10)).quantize(Decimal(".0000000001")))
+        else:
+            super().assertEqual(A, B)
+
     def test_init(self):
         t = Transaction("2000/01/01", "*", self.root)
         t.addItem("Assets", "$100")
@@ -21,12 +29,21 @@ class TransactionTest(unittest.TestCase):
 
     def test_transaction(self):
         for i in range(1, 10):
-            t = Transaction("2000/01/01", "title", self.root)
-            t.addItem("Assets", "$123")
-            t.addItem("Expenses")
-            t.commit()
-            self.assertEqual(self.root.getAccount("Assets").getValue("$"), 123 * i)
-            self.assertEqual(self.root.getAccount("Expenses").getValue("$"), -123 * i)
+            with self.subTest(i=i):
+                t = Transaction("2000/01/01", "title", self.root)
+                t.addItem("Assets", "$123")
+                t.addItem("Expenses")
+                t.commit()
+                self.assertEqual(self.root.getAccount("Assets").getValue("$"), 123 * i)
+                self.assertEqual(self.root.getAccount("Expenses").getValue("$"), -123 * i)
+
+    def test_transaction_decimal(self):
+        t = Transaction("2000/01/01", "title", self.root)
+        t.addItem("Assets", "$103.020406")
+        t.addItem("Assets", "$20.103050")
+        t.addItem("Expenses", "$-123.123456")
+        t.commit()
+        self.assertEqual(self.root.getAccount("Assets").getValue("$"), 123.123456)
 
     def test_quantity(self):
         q = 2
@@ -43,12 +60,20 @@ class TransactionTest(unittest.TestCase):
     def test_conversion(self):
         t = Transaction("2000/01/01", "title", self.root)
         t.addItem("Assets", "10 STOCK @ $100")
-        t.addItem("Expenses")
+        t.addItem("Assets")
         t.commit()
         self.assertEqual(self.root.getAccount("Assets").getValue("STOCK"), 10)
-        self.assertEqual(self.root.getAccount("Expenses").getValue("STOCK"), -10)
         self.assertEqual(self.root.getAccount("Assets").getValue("$"), -1000)
-        self.assertEqual(self.root.getAccount("Expenses").getValue("$"), 1000)
+
+    """
+    def test_out_of_order_validation(self):
+        t = Transaction("2000/01/01", "Test", self.root)
+        t.addItem("Assets", "$10")
+        t.addItem("Assets", "=$0")
+        t.addItem("Assets", "$10")
+        t.addItem("Dummy")
+        self.assertEqual(self.root.getAccount("Assets").getValue("$"), 10)
+    """
 
     def dummyAdd(self, accountName, valueStr):
         t = Transaction("2000/01/01", "Test", self.root)
@@ -62,8 +87,17 @@ class TransactionTest(unittest.TestCase):
         self.assertEqual(self.root.getAccount("Assets").getValue("$"), 110)
         self.dummyAdd("Assets", "$10")
         self.assertEqual(self.root.getAccount("Assets").getValue("$"), 120)
-        self.dummyAdd("Assets", "=$130")
-        self.assertEqual(self.root.getAccount("Assets").getValue("$"), 130)
+        self.dummyAdd("Assets", "=$160")
+        self.assertEqual(self.root.getAccount("Assets").getValue("$"), 160)
+
+    def test_validation_decimal(self):
+        self.dummyAdd("Assets", "$1.0")
+        self.dummyAdd("Assets", "$.10=$1.10")
+        self.assertEqual(self.root.getAccount("Assets").getValue("$"), 1.10)
+        self.dummyAdd("Assets", "$.10")
+        self.assertEqual(self.root.getAccount("Assets").getValue("$"), 1.20)
+        self.dummyAdd("Assets", "=$1.60")
+        self.assertEqual(self.root.getAccount("Assets").getValue("$"), 1.60)
 
     def test_post_validation(self):
         t = Transaction("2000/01/01", "Test", self.root)
@@ -80,5 +114,40 @@ class TransactionTest(unittest.TestCase):
             pass
 
 
+class ParserTest(unittest.TestCase):
+    lines = """
+2000/01/01 * Start
+	Assets:Credit					         -$50
+    Expenses:Food                           $0
+2000/01/01 Transaction 1
+    Expenses:Food                           $100
+    Assets:Credit                           =-$150
+
+2000/01/02 Transaction 2
+    Expenses:Food                           $150
+    Assets:Credit                           -$150=-$300
+; Comment
+2000/01/03 Transaction 3
+    Expenses:Food                           $300; Comment
+    Assets:Credit                           =-$600
+    """.splitlines()
+
+    def test_parse_file(self):
+        root, transactions = parse_file(self.lines)
+        assert(root.children)
+        assert(transactions)
+
+    def test_balance(self):
+        parse_args(["bal"], self.lines)
+
+    def test_register(self):
+        parse_args(["reg"], self.lines)
+
+
 if __name__ == '__main__':
+    logging.basicConfig(format='[%(filename)s:%(lineno)s]%(levelname)s:%(message)s', level=logging.DEBUG)
+    logger = logging.getLogger()
+    stream_handler = logging.StreamHandler(sys.stdout)
+    logger.addHandler(stream_handler)
+
     unittest.main()
